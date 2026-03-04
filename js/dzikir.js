7ea9b3b6e1a5814656eb8521f64e9473ba61c42a
Thobiq <malyasyithobiq@gmail.com>
2026-03-04 14:34:30 +0700

    fitur dzikir

---
+++ js/dzikir.js
@@ -0,0 +1,37 @@
+let count = 0;
+        const display = document.getElementById('angka');
+        const notif = document.getElementById('notif');
+        const targetInp = document.getElementById('target');
+
+        function tambah() {
+            count++;
+            display.innerText = count;
+            cekTarget();
+        }
+
+        function kurang() {
+            if(count > 0) {
+                count--;
+                display.innerText = count;
+                cekTarget();
+            }
+        }
+
+        function reset() {
+            count = 0;
+            display.innerText = count;
+            notif.style.display = 'none';
+        }
+
+        function cekTarget() {
+            let target = parseInt(targetInp.value);
+            if(target && count >= target && count !== 0) {
+                notif.style.display = 'block';
+            } else {
+                notif.style.display = 'none';
+            }
+        }
+
+document.getElementById('hamburger').addEventListener('click', function() {
+    document.getElementById('navLinks').classList.toggle('show');
+});
